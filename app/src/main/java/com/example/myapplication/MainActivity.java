65ca1f7ccaca0e0417cf3a8f6f16fcf87022f741
Aayushgarg71 <aayushgarg71@gmail.com>
2020-09-06 17:22:29 +0530

    Added button for addition and subtraction of quantity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,13 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    static int num=0;
+    static int num=2;
     public void submitOrder(View view){
-        num++;
+        //num++;
         display(num);
+        displayPrice(num*5);
     }
     private void display(int number){
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText(""+number);
+    }
+    private void displayPrice(int num){
+        TextView price = (TextView)  findViewById(R.id.priceview);
+                price.setText(NumberFormat.getCurrencyInstance().format(num));
+    }
+    public void increment(View view){
+        num++;
+        display(num);
+    }
+    public void decrement(View view){
+        num--;
+        display(num);
     }
 }
